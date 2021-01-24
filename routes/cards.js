087abff1e2ac1b2be9480129fb4ec9a809b7bd8e
Clucas0311 <clucas0311@gmail.com>
2021-01-24 16:30:29 -0500

    req.params

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', (req, res) => {
-	res.render('card', { prompt: "Who is buried in Grant's tomb?" });
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
+router.get('/:id', (req, res) => {
+	res.render('card', {
+		prompt: cards[req.params.id].question,
+		hint: cards[req.params.id].hint
+	});
 });
 
 module.exports = router;
